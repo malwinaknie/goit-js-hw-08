7d00462e791366b09d743c70ad9b74da607a46ec
Malwina Knie <malwinaknie@wp.pl>
2023-01-31 18:38:53 +0100

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import { throttle } from 'lodash';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+    'timeupdate',
+    throttle(() => {
+        player.getCurrentTime().then(currentTime => {
+            localStorage.setItem(STORAGE_KEY, currentTime)
+        });
+    }, 1000)
+);
+
+const onSaveTimeUpdate = localStorage.getItem(STORAGE_KEY);
+onSaveTimeUpdate === null
+? player.setCurrentTime(0)
+: player.setCurrentTime(onSaveTimeUpdate);
