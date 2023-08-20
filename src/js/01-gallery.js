546a896025a49ffb02df13f7b52394d9ded13d12
Malwina Knie <malwinaknie@wp.pl>
2023-08-20 20:06:32 +0200

    added simplelightbox and finished first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryItems.forEach(picture => {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${picture.original}">
+      <img
+        class="gallery__image"
+        src="${picture.preview}"
+        data-source="${picture.original}"
+        alt="${picture.description}"
+      />
+    </a>
+  </div>`
+  );
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  scrollZoom: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
