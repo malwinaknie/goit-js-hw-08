41a7f9671dd66d8abe4d30a1213160ae793df306
Malwina Knie <malwinaknie@wp.pl>
2023-08-22 20:24:27 +0200

    Finished task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,8 +2,13 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const vimeoPlayer = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 const KEY_STORAGE = 'videoplayer-current-time';
 
-vimeoPlayer.on('timeupdate',
-throttle())
+player.on('timeupdate', throttle(onplay, 1000));
+
+function onplay({seconds}) {
+localStorage.setItem(KEY_STORAGE, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(KEY_STORAGE) || "");
