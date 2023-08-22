0f99aecfcc6965642d1efb5cb181dc488b948dae
Malwina Knie <malwinaknie@wp.pl>
2023-08-22 17:30:16 +0200

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,9 @@
-import throttle from "lodash.throttle";
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const vimeoPlayer = new Vimeo.Player(iframe);
+const KEY_STORAGE = 'videoplayer-current-time';
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay({seconds}) {
-    localStorage.setItem('videoplayer-current-time', seconds);
-}
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+vimeoPlayer.on('timeupdate',
+throttle())
